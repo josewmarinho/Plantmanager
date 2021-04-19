ac92adea361058cbd7633d040636818b49cb499f
Jose Wellington <josewellingtonjunior@hotmail.com>
2021-04-19 16:59:53 -0300

    starting project

---
+++ src/components/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet} from 'react-native';
+import colors from '../styles/colors';
+
+interface ButtonProps {
+   title: String;  
+}
+
+export function Button({ title }: ButtonProps) {
+   return (
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+         <Text style={styles.buttonText}>
+            { title }
+            </Text>
+      </TouchableOpacity>
+   )
+}
+
+const styles = StyleSheet.create({
+   button: {
+      backgroundColor: colors.green,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 16,
+      marginBottom: 10,
+      height: 56,
+      paddingHorizontal: 10
+   },
+   buttonText: {
+      color: colors.white,
+      fontSize: 24
+   }
+})
